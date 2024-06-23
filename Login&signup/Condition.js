eff4e16009d6240abd5cdd84b1c612cdb9e680b5
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-06-23 09:29:05 +0700

    tuần 6

---
+++ Login&signup/Condition.js
@@ -0,0 +1,36 @@
+let login = document.querySelector("#login")
+login.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let username = document.getElementById('username')
+    let email = document.getElementById('email')
+    let pw = document.getElementById('password')
+
+
+    let lowerCaseLetter = /[a-z]/g
+    let upperCaseLetter = /[A-Z]/g
+    let numbers = /[0-9]/g
+
+
+
+
+    if (email.value.trim().length == 0 && pw.value.trim().length == 0) {
+        alert("Please input your email & password")
+    }
+    else if (email.value.trim().length == 0) {
+        alert("Please input your email")
+    }
+    else if (pw.value.trim().length == 0) {
+        alert("Please input your password")
+    }
+    else if (pw.value.trim().length < 8) {
+        alert("Password must be at least 8 characters.")
+    }
+    else if (!pw.value.trim().match(lowerCaseLetter)) {
+        alert("Password must  contain a lowercase letter")
+    }
+    else if (!pw.value.trim().match(upperCaseLetter)) {
+        alert("Password must  contain a uppercase letter")
+    }
+    
+
+})
