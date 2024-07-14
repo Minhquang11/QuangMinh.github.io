277432cef514092655cbbbc61b42bc2ea03613b0
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-07-14 21:05:43 +0700

    z

--- Home/Home.js
+++ Home/Home.js
@@ -1,55 +1,122 @@
-const url = 'https://hapi-books.p.rapidapi.com/nominees/romance/2020';
-const options = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': '4b4c403403msh773666ff3fef4cfp18cab5jsnfbb2aa3b82af',
-        'x-rapidapi-host': 'hapi-books.p.rapidapi.com'
-    }
-};
-async function getData() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(JSON.parse(result));
-    } catch (error) {
-        console.error(error);
-    }
+let products = {
+    GER: [
+        {
+            name: "Simon Unai",
+            position: "Goalkepper",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
 
+        {
+            name: "Marc-André ter Stegen",
+            position: "Goalkepper",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
 
-    vart
-    for(let item of JSON.parse(result)){
-        let card = document.createElement("div")
-        card.classList.add("card")
-    
-        let imgContainer = document.createElement("div")
-        imgContainer.classList.add("image-container")
-    
-        let image = document.createElement("img")
-        image.setAttribute("src", items.image)
-        imgContainer.appendChild(image)
-        card.appendChild(imgContainer)
-    
-        let container = document.createElement("div")
-        container.classList.add("container")
-    
-        let name = document.createElement("h5");
-        name.classList.add("product-name");
-        name.innerText = items.name.toUpperCase();
-        container.appendChild(name);
-    
-        let price = document.createElement("h6");
-        price.innerHTML = "<b>Price:</b> " + items.price;
-        container.appendChild(price);
-    
-        let btn = document.createElement("button")
-        btn.setAttribute("onclick", "addToCart()")
-        btn.innerHTML = "Thêm vào giỏ hàng"
-        container.appendChild(btn)
-      
-        card.appendChild(container);
-        document.getElementById("products").appendChild(card);
-    }
+        {
+            name: "Olivier Baumann",
+            position: "Goalkepper",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Waldemar Anton",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Maximilian Mittelstädtr",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Joshua Kimmich",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "David Raum",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "	Nico Schlotterbeck",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Jonathan Tah",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Robin Koch",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "	Antonio Rugiger",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Robert Andrich",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "Florian Writz",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "Emre Can",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "Leroy Sane",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "Chris Fuhrich",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "	Pascal Grob",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "	Toni Kroos",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "	Jamal Musiala",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "	Ilkay Gundogan",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+        {
+            name: "Manuel Neuer",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY",
+        },
+
+        
+    ]
 }
-getData()
-
-
