aa139f3a54985ad60f94f5c84d4633d698141ece
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-07-15 13:14:47 +0700

    add

--- Data.js
+++ Data.js
@@ -393,5 +393,193 @@ let eurofinal = {
         
 
        
+    ],
+    FRA: [
+        {
+            name: "Brice Samba",
+            age: "30",
+            match: "-",
+            position: "Goalkepper",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Mike Maignan",
+            age: "29",
+            match: "6",
+            position: "Goalkepper",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Alphonse Areola",
+            age: "31",
+            match: "-",
+            position: "Goalkepper",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Benjamin Pavard",
+            age: "28",
+            match: "-",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Ferland Mendy",
+            age: "29",
+            match: "-",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Dayot Upamecano",
+            age: "25",
+            match: "6",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Jules Koundé",
+            age: "25",
+            match: "6",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+
+        {
+            name: "William Saliba",
+            age: "23",
+            match: "6",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Jonathan Clauss",
+            age: "31",
+            match: "-",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Theo Hernández",
+            age: "26",
+            match: "6",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Ibrahima Konaté",
+            age: "25",
+            match: "-",
+            position: "Defender",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+
+        {
+            name: "Eduardo Camavinga",
+            age: "21",
+            match: "4",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Antoine Griezmann",
+            age: "33",
+            match: "6",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Aurélien Tchouameni",
+            age: "24",
+            match: "5",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "N'Golo Kanté",
+            age: "33",
+            match: "6",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Adrien Rabiot",
+            age: "29",
+            match: "5",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Warren Zaïre-Emery",
+            age: "18",
+            match: "-",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Youssouf Fofana",
+            age: "25",
+            match: "3",
+            position: "Midfielder",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Olivier Giroud",
+            age: "37",
+            match: "4",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Kylian Mbappé",
+            age: "25",
+            match: "5",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Ousmane Dembélé",
+            age: "27",
+            match: "5",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Randal Kolo Muani",
+            age: "25",
+            match: "5",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Marcus Thuram",
+            age: "26",
+            match: "4",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Kingsley Coman",
+            age: "28",
+            match: "1",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        {
+            name: "Bradley Barcola",
+            age: "21",
+            match: "3",
+            position: "Forwards",
+            image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS6kGPumyx0y2FieMSFh8upoquttFMp3B29hQDxXVwV98jryF0aJMS9pmYb5rKUq2fGlaQlWNnuHbZQlnY"
+        },
+        
     ]
 }
